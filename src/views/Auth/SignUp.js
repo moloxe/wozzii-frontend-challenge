16fd1d0717d77ec797c5abfcd4f8a8669186e3c9
Franco MG <francomolocheg99@gmail.com>
2020-08-07 22:44:00 -0500

    sign-up route

---
+++ src/views/Auth/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SignUp.css";
+
+function SignUp() {
+  return (
+    <div className="SignUp">
+      <h1>Sign Up</h1>
+    </div>
+  );
+}
+
+export default SignUp;
